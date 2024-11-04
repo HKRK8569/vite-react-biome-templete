32bbba69aacf6ed6536babcc2f3d2e1067eee9e8
HKRK8569 <kayann3021@gmail.com>
2024-11-05 01:38:36 +0900

    vite+react+biomeの環境を構築

---
+++ README.md
@@ -0,0 +1,7 @@
+# vite-react-biome-templete
+
+## 起動方法
+
+```
+npm run dev
+```
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./index.module.scss";
+
+const Button = () => {
+  return (
+    <button type="button" className={styles.button}>
+      ボタン
+    </button>
+  );
+};
+
+export default Button;
